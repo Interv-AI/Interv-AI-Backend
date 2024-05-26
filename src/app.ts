c26fb643ae1f49431771f835aa55799f1b516200
Garry028 <gmisal2002@gmail.com>
2024-05-27 00:26:07 +0530

    added cors for this

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import express from "express";
-import cors from 'cors'
+import cors from 'cors';
 import { authRouter } from "./api/routes/Auth";
 import { QuestionRouter } from "./api/routes/Question";
-const app = express()
-app.use(express.json())
-app.use(cors())
 
+const app = express();
 
-app.use("/auth", authRouter)
-app.use("/interview", QuestionRouter)
+// Configure CORS options
+const corsOptions = {
+    origin: 'https://interv-ai-frontend-pi.vercel.app',
+    optionsSuccessStatus: 200 // For legacy browser support
+};
+
+// Use CORS with the configured options
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use("/auth", authRouter);
+app.use("/interview", QuestionRouter);
 
 export default app;
